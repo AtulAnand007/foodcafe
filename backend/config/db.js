f5c62689833f1439fb35bf74ec02548e411d8623
AtulAnand007 <anandatul089@gmail.com>
2026-03-02 17:30:15 +0530

    secrets are stored safely

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://anandatul089:atulAnand@clusterfood.mt3xu.mongodb.net/?retryWrites=true&w=majority&appName=ClusterFood",
-    )
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("DB Connected at katra"));
 };
 
-// export const connectDB = async()=>{
-//     await mongoose.connect('mongodb+srv://food:Adarsh1007@cluster0.mqsmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("Mongodb connected to katra"))
-// }
+
