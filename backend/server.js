6c920459272aaaacf573828aff0a10ce582c80b9
AtulAnand007 <anandatul089@gmail.com>
2026-03-02 17:22:02 +0530

    db changes made

--- backend/server.js
+++ backend/server.js
@@ -108,7 +108,9 @@ const port = process.env.PORT || 4000;
 // Middleware for CORS
 const allowedOrigins = [
     'http://localhost:5173', // Origin 1
-    'http://localhost:5174'  // Origin 2
+    'http://localhost:5174',  // Origin 2
+    ''                     ,   // production origin 
+    ''                          // production admin origin
 ];
 
 const corsOptions = {
